6a411b8ae53af51f9d2c6b111a4b1b4c043bea0e
hprifai <harrisputra0@gmail.com>
2025-04-25 23:16:36 +0700

    change database from sqlite to mysql

--- database/migrations/2025_04_06_071145_create_posts_table.php
+++ database/migrations/2025_04_06_071145_create_posts_table.php
@@ -14,11 +14,11 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->foreignId('author_id')->constrained(
+            $table->unsignedInteger('author_id')->constrained(
                 table:'users',
                 indexName:'posts_author_id'
             );
-            $table->foreignId('category_id')->constrained(
+            $table->unsignedInteger('category_id')->constrained(
                 table:'categories',
                 indexName:'posts_category_id'
             );
